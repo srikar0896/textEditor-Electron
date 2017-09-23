e1f4bd8c39fc02b4c1866d9f82c4e3d3ff33c0fc
srikar0896 <srikar.0896@gmail.com>
2017-09-23 12:54:11 +0530

    saveas new file implementation

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,8 @@ $(function () {
   // DOM elements initialization
   var textInput = $(".textInput");
 
+	 var currentFile = null;
+
   openFile = function(){
     dialog.showOpenDialog(function (fileNames) {
 
@@ -16,7 +18,7 @@ $(function () {
       var fileName = fileNames[0];
 
         fs.readFile(fileName, 'utf-8', function (err, data) {
-			       currentFilePath = fileName;
+			       currentFile = fileName;
 			       textInput.val(data);
 
              //TODO:implement the top nav foldername,filename and filesize.
@@ -24,7 +26,7 @@ $(function () {
   	 });
     };
 
-    saveFile = function(){
+    saveAsNewFile = function(){
       var content = $("#text").val();
       var check = content.replace(/ /g, '');
       if (check === '') {
@@ -35,14 +37,28 @@ $(function () {
                   console.log("Error in detecting the path to save the file");
                   return;
               }
-              file.writeFile(savePath, content, (error) => {
+              fs.writeFile(savePath, content, (error) => {
                   if (error) console.log('File not saved; ' + error);
                   console.log('File saved at ' + savePath);
-                  
+									currentFile = savePath;
                   //TODO:implement the top nav foldername,filename and filesize.
               });
           });
       }
     };
+
+		saveFile = function(){
+		    if(currentFile === null) {
+		        saveAsNewFile();
+		    }
+		    else {
+		        var content = $("#text").val();
+		        fs.writeFile(currentFile, content, (error) => {
+		            if (error) console.log('File not saved; ' + error);
+		            console.log('File saved at ' + currentFile);
+								//TODO: implement the stats of file
+		        });
+		    }
+		};
 
 });
