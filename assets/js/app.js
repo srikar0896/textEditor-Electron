36b412f36a7d999dd871adfc15a21bbe42d0a131
Chaitanya Reddy <chavalichaithanyachinna@gmail.com>
2017-09-24 22:34:24 +0530

    New file creation

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,15 @@ $(function () {
 
 	 var currentFile = null;
 
+   formatBytes = function(bytes, decimals){
+    if (bytes == 0) return '0 Bytes';
+    var k = 1024,
+        dm = decimals || 2,
+        sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+   }
+
   updateFields = function(){
     var stats = file.statSync(currentFile);
     var fileSize = formatBytes(stats.size);
@@ -69,5 +78,13 @@ $(function () {
 		        });
 		    }
 		};
+
+    newFile = function(){
+      $("#text").val('');
+      $("#foName").html('Folder Name');
+      $("#fiName").html('File Name');
+      $("#fiSize").html('File Size');
+      currentFile = null;
+    }
 
 });
